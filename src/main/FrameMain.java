45e1acacb139869a16dc012742c8d0b3d9c61793
Jacek Strotz <31324805+eliite@users.noreply.github.com>
2022-02-09 12:29:07 -0500

    Changed Java LookAndFeel

--- src/main/FrameMain.java
+++ src/main/FrameMain.java
@@ -735,8 +735,7 @@ public class FrameMain extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            UIManager.setLookAndFeel(
-            UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel("com.apple.laf.AquaLookAndFeel");
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(FrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
